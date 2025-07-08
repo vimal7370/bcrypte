742b8d25bdce6e9e36d199510301fdc8714ee221
Vimal Kumar K <vimal7370@gmail.com>
2025-07-08 11:48:24 +0530

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -6,9 +6,9 @@ cd `dirname $0`
 
 mkdir -p ./priv;
 
-rm -f priv/libbcrypte.so;
+rm -f priv/*;
 cargo build --manifest-path=crates/bcrypte/Cargo.toml --release;
-mv crates/bcrypte/target/release/libbcrypte.so priv/.;
-
+[ -f "crates/bcrypte/target/release/libbcrypte.so" ] &&mv crates/bcrypte/target/release/libbcrypte.so priv/.;
+[ -f "crates/bcrypte/target/release/libbcrypte.dylib" ] &&mv crates/bcrypte/target/release/libbcrypte.dylib priv/.;
 
 cd $JPRE_COMPILE_PWD
